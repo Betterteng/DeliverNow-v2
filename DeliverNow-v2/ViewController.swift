56becb7eb3253ed8e7a6b7860fc9f80bb50fd81a
Betterteng <betterteng@gmail.com>
2016-05-07 01:33:56 +1000

    Initial Commit

---
+++ DeliverNow-v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeliverNow-v2
+//
+//  Created by 滕施男 on 7/05/2016.
+//  Copyright © 2016 TENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
