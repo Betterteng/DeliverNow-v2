e5563035dd4595cec5f3aad4e15d60fcc0798a62
Betterteng <betterteng@gmail.com>
2016-06-02 02:06:34 +1000

    beautify is going

--- DeliverNow-v2/ChoiceViewController.swift
+++ DeliverNow-v2/ChoiceViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class ChoiceViewController: UIViewController {
     
+    @IBOutlet var userNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUserName()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     Set userName label.
+     */
+    func setUserName() -> Void {
+        let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
+        FIREBASE_REF.observeEventType(.ChildAdded, withBlock: { snapshot in
+            let userName = (snapshot.value.objectForKey(userID)?.objectForKey("info")?.objectForKey("name"))! as! String
+            self.userNameLabel.text = userName
+        })
     }
 }
